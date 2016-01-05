66257bc3eade3732c0a7a2e0273575d1548c0305
wendy <wendy.mingui@isep.fr>
2016-01-05 13:56:03 +0100

    aide

---
+++ vue/Aide1.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+	<head>
+	<meta charset="utf-8"/>
+	<title> Sharetime</title>
+	<link rel="stylesheet" href="style_APP.css"/>
+
+	</head>
+	
+	<body>
+	<?php $formulaire= 'recherche'; 
+    include("entete.php"); 
+    include("bandeau.php");
+    $nb_even=3;?>
+	
+	<h1><span class="faq">FAQ</span></h1>
+	
+	
+	
+	
+	</body>
+</html>
