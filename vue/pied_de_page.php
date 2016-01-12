f460549a97752a7ddcc00c1733248c72e82e07b4
zhaoyue9207 <yue.zhao@isep.fr>
2016-01-12 16:55:26 +0100

    modif sur pied de page

--- vue/pied_de_page.php
+++ vue/pied_de_page.php
@@ -4,7 +4,9 @@
 			<li><a href="#"><span class="tiret">|</span>Qui somme nous? <span class="tiret">|</span></a></li>
 			<li><a href="#">Fonctionnalités <span class="tiret">|</span></a></li>
 			<li><a href="Contactez-nous.php">Nous contacter <span class="tiret">|</span></a></li>
-			<li><a href="page_conditions_generales.php">Conditions générales et confidentialité <span class="tiret">|</span></a></li>
+			<li><a href="page_conditions_generales.php">Conditions générales<span class="tiret">|</span></a></li>
+			<li><a href="page_politique_de_confidentialite.php">Politique de confidentialite<span class="tiret">|</span></a></li>
+			<li><a href="page_mention_egale.php">Mention egale<span class="tiret">|</span></a></li>
 			<li><a href="#">Langues <span class="tiret">|</span></a></li>
 		</ul>
 	<p id="reseaux_sociaux"> Rejoignez-nous sur : </p>
