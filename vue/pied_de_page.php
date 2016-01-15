7931323fa2fc67717e399bacec97bde6b5e32d68
tgindre <timothee.gindre@isep.fr>
2016-01-15 22:45:36 +0100

    Ajout d'un espace (rectification de la forme)

--- vue/pied_de_page.php
+++ vue/pied_de_page.php
@@ -1,7 +1,7 @@
 ﻿<footer>
         <a href="page_accueil.php">  <img id="logo2" src ="image/logo.png" alt="Logo sharetime"/></a>
 	<ul class="menu">
-			<li><a href="#"><span class="tiret">|</span>Qui sommes nous? <span class="tiret">|</span></a></li>
+			<li><a href="#"><span class="tiret">|</span>Qui sommes nous ? <span class="tiret">|</span></a></li>
 			<li><a href="#">Fonctionnalités <span class="tiret">|</span></a></li>
 			<li><a href="contactez-nous.php">Nous contacter<span class="tiret">|</span></a></li>
 			<li><a href="page_conditions_generales.php">Conditions générales<span class="tiret">|</span></a></li>
