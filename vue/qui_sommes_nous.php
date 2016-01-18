44c2ba226ec079af18ca50a92854795c505167e6
loup1000 <jean-loup.robin@isep.fr>
2016-01-18 23:02:37 +0100

    fini qui sommes nous

--- vue/qui_sommes_nous.php
+++ vue/qui_sommes_nous.php
@@ -11,16 +11,18 @@
 	<?php $formulaire= 'recherche'; 
     include("entete.php"); 
     include("nom.php");
- ?>
+    ?>   <div class="presentation"><h1>Qui sommes nous</h1>
         <p>Une association à but non lucratif a pour objectif de proposer un site web permettant aux utilisateurs de proposer,
         promouvoir et de participer à des événements en tout genre tels que pique-niques géants,concerts,spectaclesdedanse, brocantes, expositions, conférences, etc.</p>
         <h1>Fondateurs</h1>
         <h2>Matthieu Manceny</h2>
         <div><p>Enseigant chercheur en informatique, docteur en bioinformatique/ingénieur en informatique</p>
             <p>Matthieu Mancency est le fondateur de ShareTime.<br/>
-            Entrepreneur depuis quelques années, il a tout d'abord obtenu un doctorat</p></div>
+            Entrepreneur depuis quelques années, il a tout d'abord obtenu un doctorat en Bioinformatique et conduis une thèse sur 
+            "l'assurance qualité sur les systèmes M2M : Application aux passerelles et algorythme Multimédia Adaptables"<br/>
+            Il met en place ses compétences en Informatiques afin de créer une plateforme de partage</p></div>
+    </div>
         
-        <h1>Qui sommes nous</h1>
                	
 	 <?php
         include("pied_de_page.php"); ?>
