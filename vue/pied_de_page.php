ceb2af6320cf7b2c906ce210cf58fc81ca33015e
tgindre <timothee.gindre@isep.fr>
2016-01-18 17:57:39 +0100

    Retrait de l'onglet fonctionnalités

--- vue/pied_de_page.php
+++ vue/pied_de_page.php
@@ -2,7 +2,6 @@
         <a href="page_accueil.php">  <img id="logo2" src ="image/logo.png" alt="Logo sharetime"/></a>
 	<ul class="menu">
 			<li><a href="#"><span class="tiret">|</span>Qui sommes-nous ? <span class="tiret">|</span></a></li>
-			<li><a href="#">Fonctionnalités <span class="tiret">|</span></a></li>
 			<li><a href="contactez-nous.php">Nous contacter<span class="tiret">|</span></a></li>
 			<li><a href="page_conditions_generales.php">Conditions générales<span class="tiret">|</span></a></li>
 			<li><a href="page_politique_de_confidentialite.php">Politique de confidentialité<span class="tiret">|</span></a></li>
